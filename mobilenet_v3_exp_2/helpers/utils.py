138b6db1f1d01787cf42001d4a5021086fbe3557
TTeuZ <pmla.luza@gmail.com>
2024-02-26 23:48:07 -0300

    [wip] Repo refactor

---
+++ mobilenet_v3_exp_2/helpers/utils.py
@@ -0,0 +1,29 @@
+import numpy as np
+import torch
+
+def fast_collate(batch):
+    images = [image[0] for image in batch]
+    targets = torch.tensor([target[1] for target in batch], dtype=torch.float32)
+
+    width, height = 128, 128
+    tensor = torch.zeros((len(images), 3, height, width), dtype=torch.float32).contiguous()
+
+    for index, image in enumerate(images):
+        nump_array = np.asarray(image, dtype=np.float32)
+
+        if(nump_array.ndim < 3):
+            nump_array = np.expand_dims(nump_array, axis=-1)
+
+        nump_array = np.rollaxis(nump_array, 2)
+        tensor[index] += torch.from_numpy(nump_array)
+
+    return tensor, targets
+
+
+def print_confusion_matrix(cm):
+    print("Confusion matrix:")
+    
+    for i in reversed(range(2)):
+        for j in reversed(range(2)):
+            print(cm[i][j], end=' ')
+        print('\n', end='')

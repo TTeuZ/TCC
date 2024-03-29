3d6f421c6e1ceca1d5376313e01eeca67d3fa4b6
TTeuZ <pmla.luza@gmail.com>
2024-03-28 22:04:07 -0300

    [add] standard deviation in the summary files

--- main/summary.py
+++ main/summary.py
@@ -25,6 +25,13 @@ def print_results(results, file):
     avg_accuracy = sum(accuracies) / len(accuracies)
     avg_cm = np.round(sum(cms) / len(cms))
 
+    std_auc = np.std(aucs)
+    std_eer = np.std(eers)
+    std_threshold = np.std(thresholds)
+    std_val_loss = np.std(val_loss)
+    std_epoch = np.std(epochs)
+    std_accuracy = np.std(accuracies)
+
     file.write(f"Train: {train_ds} - Test: {test_ds}\n")
     file.write("\n-------------------------[ALL RESULTS]-------------------------\n")
     file.write(" - ".join(f"{auc:.4f}" for auc in aucs) + " [AUCS]\n")
@@ -35,32 +42,36 @@ def print_results(results, file):
     file.write(" - ".join(f"{acc:.4f}" for acc in accuracies) + " [ACCURACY]\n")
 
     file.write("\n-------------------------[AVG RESULT]--------------------------\n")
-    file.write(f"{avg_auc:.4f} [AUC] \t\t\t\t\t  Confusion matrix - Rounded\n")
-    file.write(f"{avg_eer:.4f} [EER] \t\t\t\t\t\t\t  {avg_cm[1][1]:6.0f} \t  {avg_cm[1][0]:6.0f}\n")
-    file.write(f"{avg_threshold:.4f} [THRESHOLD] \t\t\t\t\t\t  {avg_cm[0][1]:6.0f} \t  {avg_cm[0][0]:6.0f}\n")
-    file.write(f"{avg_val_loss:.4f} [VAL LOSS]\n")
-    file.write(f"{avg_epoch:6d} [EPOCH]\n")
-    file.write(f"{avg_accuracy:.4f} [ACCURACY]\n")
+    file.write(" [AVG]  [STD] \t\t\t\t\t  Confusion matrix - Rounded\n")
+    file.write(f"{avg_auc:.4f} {std_auc:.4f} [AUC] \t\t\t\t\t  {avg_cm[1][1]:6.0f} \t  {avg_cm[1][0]:6.0f}\n")
+    file.write(f"{avg_eer:.4f} {std_eer:.4f} [EER] \t\t\t\t\t  {avg_cm[0][1]:6.0f} \t  {avg_cm[0][0]:6.0f}\n")
+    file.write(f"{avg_threshold:.4f} {std_threshold:.4f} [THRESHOLD]\n")
+    file.write(f"{avg_val_loss:.4f} {std_val_loss:.4f} [VAL LOSS]\n")
+    file.write(f"{avg_epoch:6d} {std_epoch:.4f} [EPOCH]\n")
+    file.write(f"{avg_accuracy:.4f} {std_accuracy:.4f} [ACCURACY]\n")
 
     file.write("\n-------------------------[ BY SUBSET ]-------------------------\n")
     subsets = set()
     for subset in results[0]["test"]["subsets"]:
         subsets.add(subset)
     
-    results_by_subset = {subset: {'accuracy': 0.0, 'cm': [[0, 0], [0, 0]]} for subset in subsets}
+    results_by_subset = {subset: {'accuracy': 0.0, 'std_accuracy': [], 'cm': [[0, 0], [0, 0]]} for subset in subsets}
     results_by_subset = dict(sorted(results_by_subset.items()))
 
     for result in results:
         for subset in result["test"]["subsets"]:
             results_by_subset[subset]["accuracy"] += result["test"]["subsets"][subset]["accuracy"]
+            results_by_subset[subset]["std_accuracy"].append(result["test"]["subsets"][subset]["accuracy"])
             results_by_subset[subset]["cm"] += get_cm(result["test"]["subsets"][subset]["cm"])
 
     for subset in results_by_subset:
         results_by_subset[subset]["accuracy"] = results_by_subset[subset]["accuracy"] / len(accuracies) # Any array would work
+        results_by_subset[subset]["std_accuracy"] = np.std(results_by_subset[subset]["std_accuracy"])
         results_by_subset[subset]["cm"] = np.round(results_by_subset[subset]["cm"] / len(cms))
     
     for subset in results_by_subset:
         accuracy = results_by_subset[subset]["accuracy"]
+        std_accuracy = results_by_subset[subset]["std_accuracy"]
         cm = results_by_subset[subset]["cm"]
 
         file.write(f"{subset}:".ljust(37))
@@ -68,7 +79,7 @@ def print_results(results, file):
 
         file.write(f"{accuracy:.4f} [ACCURACY]".ljust(47))
         file.write(f"{cm[1][1]:6.0f} \t  {cm[1][0]:6.0f}\n")
-        file.write("".ljust(47))
+        file.write(f"{std_accuracy:.4f} [STD_ACCURACY]".ljust(47))
         file.write(f"{cm[0][1]:6.0f} \t  {cm[0][0]:6.0f}\n")
         file.write("\n")
 

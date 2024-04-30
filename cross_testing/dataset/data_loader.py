b7485e67f8d7971eb63b0623fef6dc1f6649d5e7
TTeuZ <pmla.luza@gmail.com>
2024-04-30 19:18:50 -0300

    [add] thread pool to load images

--- cross_testing/dataset/data_loader.py
+++ cross_testing/dataset/data_loader.py
@@ -2,6 +2,7 @@ import sys, os; sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 from torchvision import transforms, datasets
 from tools.dataset.memory_dataset import memory_dataset
+from multiprocessing import Pool
 from datetime import datetime
 import numpy as np
 import cv2 as cv
@@ -15,23 +16,45 @@ class data_loader():
         self.transform = transforms.Compose([transforms.Resize(config["img_size"]), transforms.ToTensor()])
 
 
-    def __get_dataset(self, paths):
-        images, targets = [], []
+    def _load_image(self, image_path):
+        return cv.imread(image_path)
+
+
+    def _process_directory(self, args):
+        path, class_name = args
+
+        images = []
+        for image in os.listdir(path):
+            image_path = f"{path}/{image}"
+            images.append((image_path, class_name))
         
+        return images
+
+
+    def _get_dataset(self, paths):
+        all_paths, images, targets = [], [], []
+
         for _, path in paths:
             for class_name in os.listdir(path):
-                full_path = f"{path}/{class_name}"
-                for image in os.listdir(full_path):
-                    image_path = f"{full_path}/{image}"
-                    image = cv.imread(image_path)
-                    
-                    images.append(image), targets.append(CLASSES[class_name])
-        
-        targets = np.array(targets)
+                all_paths.append((f"{path}/{class_name}", class_name))
+
+        pool = Pool()
+        results = pool.map(self._process_directory, all_paths)
+        pool.close(), pool.join()
+
+        image_paths = [item for sublist in results for item in sublist]
+
+        pool = Pool()
+        images = pool.map(self._load_image, [item[0] for item in image_paths])
+        pool.close(), pool.join()
+
+        targets = [CLASSES[item[1]] for item in image_paths]
+
+        targets =  np.array(targets)
         return memory_dataset(images, targets, self.transform)
 
 
-    def __get_dates(self, ds_path):
+    def _get_dates(self, ds_path):
         dates = os.listdir(ds_path)
         formated_dates = [(datetime.strptime(date, '%Y-%m-%d').date(), f"{ds_path}/{date}") for date in dates]
         
@@ -45,16 +68,16 @@ class data_loader():
         
         first_half, second_half = [], []
         for subset in subsets:
-            dates = self.__get_dates(subset)
+            dates = self._get_dates(subset)
 
             divisor = math.ceil(len(dates) * t_size)
             first_half.extend(dates[:divisor])
             second_half.extend(dates[divisor:])
     
         if len(second_half) == 0:
-            return self.__get_dataset(first_half)
+            return self._get_dataset(first_half)
         else:
-            return (self.__get_dataset(first_half), self.__get_dataset(second_half))
+            return (self._get_dataset(first_half), self._get_dataset(second_half))
         
 
     def load_dataset_by_subset(self, ds_path=""):
@@ -64,7 +87,7 @@ class data_loader():
         subsets = os.listdir(ds_path)
 
         for subset in subsets:
-            dates = self.__get_dates(f"{ds_path}/{subset}")
-            datasets[subset] = self.__get_dataset(dates)
+            dates = self._get_dates(f"{ds_path}/{subset}")
+            datasets[subset] = self._get_dataset(dates)
 
         return datasets

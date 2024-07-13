e6a22e39f1248ff5a9500444c11596a34d51d78b
TTeuZ <pmla.luza@gmail.com>
2024-07-13 11:24:34 -0300

    updating configs and run.py

--- pipeline_days/run.py
+++ pipeline_days/run.py
@@ -10,16 +10,26 @@ import json
 EXP_UUID = uuid.uuid4()
 EXP_NAME = f"exp_{EXP_UUID}"
 
+def get_related_models_json(path, dataset):
+    jsons = os.listdir(f"{path}/jsons")
+    jsons = [json.load(open(f"{path}/jsons/{file}", "r")) for file in jsons]
+    jsons = [file for file in jsons if dataset in file["dataset"]["train"]]
+
+    return jsons
+
+
 def main(args):
     assert os.path.exists(args.config), "Invalid config"
 
     begin_datetime = "--".join(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S').split(" "))
 
     config = json.load(open(args.config, "r"))
-    fathers_json = config['fathers']
+    fathers_config = config['fathers']
     initial_weights = config["initial_weights"]
     subsets = sorted(os.listdir(config["dataset"]["path"]))
 
+    father_jsons = get_related_models_json(fathers_config["models"]["path"], fathers_config["models"]["dataset"])
+    initial_jsons = get_related_models_json(initial_weights["path"], initial_weights["dataset"])
     generator = ensemble_generator(config, EXP_NAME)
 
     create_folder("_summaries")
@@ -32,12 +42,12 @@ def main(args):
         file.write("# Experiment infos\n\n")
 
         file.write("## Fathers\n")
-        file.write(f"- Model: {fathers_json['module']}\n")
-        file.write(f"- trained_At: {fathers_json['trained_at']}\n")
-        file.write(f"- Training mode: {fathers_json['config']['training_mode']}\n")
+        file.write(f"- Model: {fathers_config['module']}\n")
+        file.write(f"- Training mode: {fathers_config['config']['training_mode']}\n")
+        file.write(f"- Trained at: {fathers_config['trained_at']}\n")
         file.write("- Models used:\n")
-        for father in fathers_json["models"]:
-            file.write(f"    - {father}\n")
+        for father in father_jsons:
+            file.write(f"    - {father['best_model']['model']}\n")
 
         file.write("\n## Sons\n")
         file.write(f"- Model: {config['model']['module']}\n")
@@ -56,12 +66,15 @@ def main(args):
         file.write(f"- Sumary: _summaries/summary_{EXP_UUID}\n")
 
     # Running pipeline
-    for index, father in enumerate(fathers_json["models"]):
-        create_folder(f"_results/{EXP_NAME}/{father.split('/')[-1][:-3]}")
+    for index, father in enumerate(father_jsons):
+        father_model = f"{fathers_config['models']['path']}/models/{father['best_model']['model']}"
+        create_folder(f"_results/{EXP_NAME}/{father_model.split('/')[-1][:-3]}")
 
-        generator.generate(father)
+        generator.generate(father_model)
         for subset in subsets:
-            os.system(f"python3 main.py -f {father} -i {initial_weights[index]['name']} -it {initial_weights[index]['threshold']} -su {subset} -c {args.config} -e {EXP_NAME}")
+            base_model = f"{initial_weights['path']}/models/{initial_jsons[index]['best_model']['model']}"
+            initial_threshold = initial_jsons[index]["metrics"]["threshold"]
+            os.system(f"python3 main.py -f {father_model} -i {base_model} -it {initial_threshold} -su {subset} -c {args.config} -e {EXP_NAME}")
 
     end_datetime = "--".join(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S').split(" "))
 
